2271913c860e1596f6ef26c6b2eda63853920cf4
hbhdytf <hbhdyangtengfei@163.com>
2014-03-31 23:59:41 +0800

    choose

--- func.cpp
+++ func.cpp
@@ -1,8 +1,8 @@
 /*************************************************************************
-	> File Name: func.cpp
-	> Author: sandy
-	> Mail: sandy@luo.bo 
-	> Created Time: 2014年03月31日 星期一 16时22分35秒
+  > File Name: func.cpp
+  > Author: sandy
+  > Mail: sandy@luo.bo 
+  > Created Time: 2014年03月31日 星期一 16时22分35秒
  ************************************************************************/
 
 #include<iostream>
@@ -13,25 +13,25 @@ using namespace std;
 
 string chooseIris(char* ResulTxt)
 {
-    
+
 }
 
 int main(int argc,char *argv[])
 {
-//    char* filename="/home/sandy/Iris/IRIS4/scores/list_result_matching_inter.txt";
+	//    char* filename="/home/sandy/Iris/IRIS4/scores/list_result_matching_inter.txt";
 	const char* filename="/home/sandy/list_result_matching_inter.txt";
-    ifstream infile(filename);
-    if(!infile)
-    {
-        cerr << "Could not open files!" << endl;
-        return -1;
-    }
-    string file1,file2,score;
+	ifstream infile(filename);
+	if(!infile)
+	{
+		cerr << "Could not open files!" << endl;
+		return -1;
+	}
+	string file1,file2,score;
 	map < string, double > mapS; 
 	map < string, double >::iterator iter;
 	double temp;
-    while(infile>>file1>>file2>>score)
-    {
+	while(infile>>file1>>file2>>score)
+	{
 		//find element
 		iter = mapS.find(file1);
 		if(iter != mapS.end())
@@ -43,14 +43,14 @@ int main(int argc,char *argv[])
 		{
 			mapS.insert(make_pair(file1,atof(score.c_str())));
 		}
-        //cout<<file1<<" "<<file2<<" "<<score<<endl;
-    }
+		//cout<<file1<<" "<<file2<<" "<<score<<endl;
+	}
 	map<string,double>::iterator tempit;
 	for(tempit=mapS.begin();tempit!=mapS.end();tempit++)
 	{
 		cout<<tempit->first<<"--"<<tempit->second<<endl;
 	}
-    infile.close();
+	infile.close();
 
-    return 0;
+	return 0;
 }
